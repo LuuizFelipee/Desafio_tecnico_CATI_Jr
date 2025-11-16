447ca5380826b08e25abed09c46a80e9fb508028
Luiz Felipe F. de França <111398965+LuuizFelipee@users.noreply.github.com>
2025-11-16 14:26:40 -0300

    Swap x and y axes in box plot visualization

--- dashboard.py
+++ dashboard.py
@@ -71,10 +71,9 @@ with col_graf1:
 with col_graf2:
   grafico_boxplot2 = px.box(
       df,
-      x='PrecoVenda',
-      y='QualidadeGeral',
-      title='Distribuição de Preços por Qualidade',
-      color='QualidadeGeral'
+      x='QualidadeGeral',
+      y='PrecoVenda',
+      title='Distribuição de Preços por Qualidade'
   )
   grafico_boxplot2.update_layout(title_x=0.1)
   st.plotly_chart(grafico_boxplot2, use_container_width=True)
